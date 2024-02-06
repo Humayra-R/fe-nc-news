4ca4d55683d55e9d94da6618b16b9d9efa013627
Humayra-R <humayra@Humayra>
2024-02-06 16:08:47 +0000

    fe-6-view-article-comments-complete

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,35 +1,35 @@
-// import { useEffect, useState } from "react"
-// import ApiRequest from "./ApiRequest"
-// import CommentsList from "./CommentsList"
+import { useEffect, useState } from "react"
+import ApiRequest from "./ApiRequest"
+import CommentsList from "./CommentsList"
 
 
-// export default function Comments({ article_id, username }) {
-//     const [comments, setComments] = useState([])
-//     const [isLoading, setIsLoading] = useState(false)
-//     const [errMsg, setErrMsg] = useState(null)
+export default function Comments({ article_id }) {
+    const [comments, setComments] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [errMsg, setErrMsg] = useState(null)
 
-//     useEffect(() => {
-//         setIsLoading(true)
-//         ApiRequest(`/articles/${article_id}/comments`)
-//         .then((data) => {
-//             const { comments } = data
-//             setComments(comments)
-//             setIsLoading(false)
-//         })
-//         .catch((err) => {
-//             setIsLoading(false)
-//             const { msg } = err.response.data
-//             setErrMsg(msg)
-//         })
-//     }, [article_id])
+    useEffect(() => {
+        setIsLoading(true)
+        ApiRequest(`/articles/${article_id}/comments`)
+        .then((data) => {
+            const { comments } = data
+            setComments(comments)
+            setIsLoading(false)
+        })
+        .catch((err) => {
+            setIsLoading(false)
+            const { msg } = err.response.data
+            setErrMsg(msg)
+        })
+    }, [article_id])
 
-//     if (isLoading) return <p className="intermediary"> ... loading comments </p>
+    if (isLoading) return <p className="intermediary"> ... loading comments </p>
 
-//     if (errMsg) return <p className="intermediary"> {errMsg} </p>
+    if (errMsg) return <p className="intermediary"> {errMsg} </p>
 
-//     return (
-//         <div>
-//             <CommentsList comments={ comments } />
-//         </div>
-//     ) 
-// }
+    return (
+        <div>
+            <CommentsList comments={ comments } />
+        </div>
+    ) 
+}
