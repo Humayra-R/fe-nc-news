357943340fe1ff93dd6962c4ebc7c2a7c3ba065d
Humayra-R <humayra@Humayra>
2024-02-07 19:26:14 +0000

    fe-7-vote-on-an-article-progress

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,4 +1,36 @@
+import { useEffect, useState } from 'react'
+import axios from "axios"
+
 export default function ArticleList({ article }) {
+    const [ votes, setVotes ] = useState(0)
+    const [ errMsg, setErrMsg ] = useState(null)
+
+    useEffect(() => {
+        setVotes(article.votes)
+    }, [])
+
+    const handleClick = (num) => {
+
+        setVotes((currVotes) => {
+            currVotes + num
+        })
+        // axios.patch(`https://nc-news-xrc9.onrender.com/api/articles/${article.article_id}`, { inc_votes: num })
+        .then(({ data }) => {
+            const [ updatedArticle ] = data.updatedArticle
+            // setVotes(updatedArticle.votes)
+        })
+        .catch((err) => {
+            setVotes((currentVotes) => {
+                currentVotes - num
+            })
+            const { msg } = err.response.data
+            setErrMsg(msg)
+        })
+       
+    }
+
+    if (errMsg) return <p> ERROR: {errMsg} </p>
+
     return (
         <main>
             <img src={article.article_img_url} alt="article image"/>
@@ -7,7 +39,11 @@ export default function ArticleList({ article }) {
             <p> Topic: {article.topic} </p>
             <p> {article.body} </p>
             <p> Article ID: {article.article_id} </p>
-            <p> Votes: {article.votes} </p>
+            <p>
+                <button onClick={() => handleClick(-1)} aria-label='button for subtracting a vote from the article'> - </button>
+                Votes: { votes }
+                <button onClick={() => handleClick(1)} aria-label='button for adding a vote to the article'> + </button>
+            </p>
             <p> Comment count: {article.comment_count} </p>
         </main>
     )
